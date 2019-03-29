656ecfe4f45dc7dc5a085b77fc7786ad71cb3670
anacollado <34594754+anacollado@users.noreply.github.com>
2019-03-29 18:30:34 -0500

    Populate db with 100 fake entries

--- client/src/components/RecommendedAlbums.jsx
+++ client/src/components/RecommendedAlbums.jsx
@@ -25,7 +25,7 @@ class RecommendedAlbums extends React.Component {
         return <div key={album.id} className="album-square" onMouseEnter={() => this.enterHandler(album.id)} onMouseLeave={this.leaveHandler}> <img className="album-art" src={album.albumArt} />
           <span>{album.albumName}</span>
           <p>by {album.artist}</p>
-          { this.state.activeAlbum === album.id ? <div>{album.description}</div> : null }
+          { this.state.activeAlbum === album.id ? <div className="album-description">{album.description}</div> : null }
         </div>
 
       })
