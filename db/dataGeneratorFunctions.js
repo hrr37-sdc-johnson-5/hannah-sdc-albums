b487634d3e39708933e36e9bf64272ae98f9fb21
Hannah Kim <hannahsohkim@gmail.com>
2019-04-18 16:02:28 -0700

    Create functions to generate data

--- db/dataGeneratorFunctions.js
+++ db/dataGeneratorFunctions.js
@@ -1,15 +1,11 @@
 var faker = require('faker');
-const mongoose = require('mongoose');
-const model = require('./db.js');
-mongoose.connect('mongodb://localhost/fetcher');
 
 
-
-
-var fakeTags = () => {
+var createTags = () => {
   var tags = [];
   var numOfTags = faker.random.number({'min': 1, 'max': 3});
   var genres = ['Art Punk', 'Alternative Rock', 'Indie Rock', 'Classic Blues', 'Swamp Blues', 'Sing-Along', 'Baroque', 'Impressionist', 'Symphonic', 'Novelty', 'Country Rock', 'Honky Tonk', 'Progressive', 'Deep House', 'Trance', 'Background', '8bit', 'Ambient', 'Vaporwave', 'Folk', 'Indie Pop', 'Gospel', 'K-Pop', 'Latin Jazz', 'Bebop', 'Bossa Nova', 'Disco', 'Contemporary R&B', 'Soul', 'Ska', 'Psychedelic', 'Glam Rock', 'Indie Folk', 'Soundtrack', 'World', 'Adult Alternative', 'Emo', 'Screamo', 'Spoken Word', 'Blues Rock', 'Electro', 'Dance', 'Lo-fi', 'Grunge', 'New Wave', 'Twee Pop', 'Memphis Blues', 'Cantata', 'Gregorian Chant', 'Renaissance', 'Americana', 'Bluegrass', 'Dreampop', 'Zydeco', 'Lounge', 'Dubstep', 'Trap', 'Rap', 'Hip-hop', 'Crunk', 'Freak Folk', 'Noise', 'Instrumental', 'Big Band', 'Hard Bop', 'Soft Rock', 'Synth Pop', 'Reggae', 'Art Rock', 'Math Rock'];
+
   while(tags.length < numOfTags) {
     var index = Math.floor(Math.random() * genres.length);
     if (!tags.includes(genres[index])) {
@@ -19,41 +15,22 @@ var fakeTags = () => {
   return tags.join(', ');
 }
 
-
-
-var makeData = () => {
-  var dataArr = [];
-  for (var i = 1; i < 100; i++) {
-    var singleEntry = {};
-    singleEntry.id = i + 1;
-    singleEntry.albumName = `${faker.commerce.color()} ${faker.lorem.word()}`;
-    singleEntry.artist = `${faker.name.firstName()}`;
-    singleEntry.albumArt = `https://s3.us-east-2.amazonaws.com/album-art-bandland/album-art-assets/picsum-${i}.jpg`;
-    singleEntry.tags = fakeTags();
-    singleEntry.description = faker.lorem.sentences(2);
-    dataArr.push(singleEntry);
-  }
-  return dataArr;
-}
-
-var dataEntries = makeData();
-
-var dummy1 =  {id: 1,
-albumName: `${faker.commerce.color()} ${faker.lorem.word()}`,
-artist: `DerAnaM`,
-albumArt: `https://s3.us-east-2.amazonaws.com/album-art-bandland/album-art-assets/picsum-${1}.jpg`,
-tags: fakeTags(),
-description: faker.lorem.sentences(2)
-}
-
-dataEntries.push(dummy1);
-
-// to drop collection, uncomment:
-
-// model.Album.remove({}, function(err) {
-//   console.log('collection removed')
-// });
-
-model.Album.insertMany(dataEntries, function (err, docs) {
-  if (err) return err;
+const createAlbum = () => ({
+  name: faker.commerce.word(2),
+  artist: faker.name.firstName(),
+  tags: createTags(),
+  image: faker.image.abstract(),
+  description: faker.lorem.sentences(2),
 });
+
+const createAllAlbums = () => {
+  var albums = [];
+  for (var i = 0; i < 10000000; i++) {
+    let newAlbum = createAlbum();
+    albums.push(newAlbum);
+  }
+  return albums;
+}
+
+var dataEntries = createAllAlbums();
+
