7f9236c0e53d131b257a90cd0f3f4cd1b9cb81ff
Hannah Kim <hannahsohkim@gmail.com>
2019-04-20 16:06:44 -0700

    Delete knex files

--- db/seeds/albums_albums.js
+++
@@ -1,8 +0,0 @@
-const db = require('../generateData.js');
-const csv = require('csv-parser');
-const fs = require('fs');
-
-exports.seed = async function(knex, Promise) {
-  // Deletes ALL existing entries
-
-};
