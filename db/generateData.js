bb21f68f5cd8bb70fce262a3d549d9bf4a74748c
Hannah Kim <hannahsohkim@gmail.com>
2019-04-20 16:15:10 -0700

    Add comment to explain createTags function and rename variables

--- db/generateData.js
+++ db/generateData.js
@@ -4,15 +4,16 @@ const faker = require('faker');
 
 const writer = csvWriter();
 
+// function to generate 1-3 tags that are randomly selected
 const createTags = () => {
   const tags = [];
   const numOfTags = faker.random.number({ min: 1, max: 3 });
   const genres = ['Art Punk', 'Alternative Rock', 'Indie Rock', 'Classic Blues', 'Swamp Blues', 'Sing-Along', 'Baroque', 'Impressionist', 'Symphonic', 'Novelty', 'Country Rock', 'Honky Tonk', 'Progressive', 'Deep House', 'Trance', 'Background', '8bit', 'Ambient', 'Vaporwave', 'Folk', 'Indie Pop', 'Gospel', 'K-Pop', 'Latin Jazz', 'Bebop', 'Bossa Nova', 'Disco', 'Contemporary R&B', 'Soul', 'Ska', 'Psychedelic', 'Glam Rock', 'Indie Folk', 'Soundtrack', 'World', 'Adult Alternative', 'Emo', 'Screamo', 'Spoken Word', 'Blues Rock', 'Electro', 'Dance', 'Lo-fi', 'Grunge', 'New Wave', 'Twee Pop', 'Memphis Blues', 'Cantata', 'Gregorian Chant', 'Renaissance', 'Americana', 'Bluegrass', 'Dreampop', 'Zydeco', 'Lounge', 'Dubstep', 'Trap', 'Rap', 'Hip-hop', 'Crunk', 'Freak Folk', 'Noise', 'Instrumental', 'Big Band', 'Hard Bop', 'Soft Rock', 'Synth Pop', 'Reggae', 'Art Rock', 'Math Rock'];
 
   while (tags.length < numOfTags) {
-    const index = Math.floor(Math.random() * genres.length);
-    if (!tags.includes(genres[index])) {
-      tags.push(genres[index]);
+    const tagIndex = Math.floor(Math.random() * genres.length);
+    if (!tags.includes(genres[tagIndex])) {
+      tags.push(genres[tagIndex]);
     }
   }
   return tags.join(', ');
