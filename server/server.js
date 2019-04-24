bc4dc0d451a535d352975225333868b0438e65f1
Hannah Kim <hannahsohkim@gmail.com>
2019-04-23 22:12:14 -0700

    Add airbnb styling

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const parser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const router = require('./router.js');
+
 const app = express();
 const port = 3001;
-const router = require('./router.js');
-const cors = require('cors')
 
 app.use(cors());
-
 app.use(parser.json());
-
-app.use('/', express.static(__dirname + '/../client/dist'));
-app.use('/:id', express.static(__dirname + '/../client/dist'));
-
+app.use('/', express.static(path.join(__dirname, '/../client/dist')));
+app.use('/:id', express.static(path.join(__dirname, '/../client/dist')));
 app.use('/api', router);
 
 
