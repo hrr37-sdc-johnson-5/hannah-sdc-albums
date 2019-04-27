67ce0a5c49dc6790105bb72b5ea373703b947090
Hannah Kim <hannahsohkim@gmail.com>
2019-04-27 10:26:36 -0700

    Add callbacks for all queries

--- db/index.js
+++ db/index.js
@@ -4,25 +4,23 @@ const { pool } = require('./config.js');
 const getAlbumsByAlbumId = (album_id, callback) => {
   pool.query('select * from albums where album_id = $1', [album_id])
     .then((albums) => {
-      console.log(albums);
       callback(albums);
     })
-    .catch(err => console.log('Error getting all albums'));
+    .catch((err) => {
+      console.log(err, 'Error getting all albums');
+      callback(err);
+    });
 };
 
-
-// const getAlbumsByAlbumId = (album_id) => {
-//   return new Promise((resolve, reject) => {
-//     pool.query('select * from albums where album_id = $1', [album_id], (err, result) => {
-//       if (err) {
-//         reject(err);
-//       } else {
-//         resolve(result.rows[0]);
-//       }
-//     });
+// const getAlbumsByAlbumId = (req, res) => {
+//   const id = req.params.id
+//   pool.query('select * from albums where album_id = $1', [id], (error, albums) => {
+//     if (error) {
+//       console.log(error);
+//     }
+//     res.status(200).send(albums);
 //   });
 // };
-
 
 // const getAlbumById = (id) => {
 //   return new Promise((resolve, reject) => {
@@ -45,53 +43,42 @@ const getAlbumsByAlbumId = (album_id, callback) => {
 // };
 
 
-const addAlbum = (album_id, name, artist, image, tags, description) => {
-  return new Promise((resolve, reject) => {
-    const params = [album_id, name, artist, image, tags, description];
-    pool.query('insert into albums values ($1, $2, $3, $4, $5, $6)', params, (err, result) => {
-      if (err) { reject(err); }
-      resolve(result);
+const addAlbum = (album_id, name, artist, image, tags, description, callback) => {
+  const params = [album_id, name, artist, image, tags, description];
+  pool.query('insert into albums values ($1, $2, $3, $4, $5, $6)', params)
+    .then((newAlbum) => {
+      callback(newAlbum);
+    })
+    .catch((err) => {
+      console.log(err, 'Error adding new album');
+      callback(err);
     });
-  });
 };
 
 // UPDATE
-// const updateAlbum = (id, toUpdate) => {
-//   pool.query(`update albums set ${toUpdate}= $1 where id = $2`, [id, toUpdate])
-//     .then((updatedAlbum) => {
-//       console.log(updatedAlbum);
-//       return updatedAlbum;
-//     })
-//     .catch(err => console.log(err, 'Error updating album'));
-// };
-
-const updateAlbum = (id, toUpdate, value) => {
-  return new Promise((resolve, reject) => {
-    pool.query(`update albums set ${toUpdate}= $1 where id = $2`, [value, id], (err, result) => {
-      if (err) { reject(err); }
-      resolve(result);
+const updateAlbum = (id, toUpdate, value, callback) => {
+  pool.query(`update albums set ${toUpdate}= $1 where id = $2`, [value, id])
+    .then((updatedAlbum) => {
+      callback(updatedAlbum);
+    })
+    .catch((err) => {
+      console.log(err, 'Error updating album');
+      callback(err);
     });
-  });
 };
 
 // DELETE
-// const deleteAlbum = (id) => {
-//   pool.query('delete from albums where id = $1', id)
-//     .then((deletedAlbum) => {
-//       console.log(deletedAlbum);
-//       return deletedAlbum;
-//     })
-//     .catch(err => console.log(err, 'Error deleting album'));
-// };
-
-const deleteAlbum = (id) => {
-  return new Promise((resolve, reject) => {
-    pool.query('delete from albums where id = $1', id, id, (err, result) => {
-      if (err) { reject(err); }
-      resolve(result);
+const deleteAlbum = (id, callback) => {
+  pool.query('delete from albums where id = $1', id)
+    .then((deletedAlbum) => {
+      callback(deletedAlbum);
+    })
+    .catch((err) => {
+      console.log(err, 'Error deleting album');
+      callback(err);
     });
-  });
 };
+
 
 module.exports = {
   getAlbumsByAlbumId,
