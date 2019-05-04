cd624be66aa6d3f37a214683486b625ae06fc499
Hannah Kim <hannahsohkim@gmail.com>
2019-05-03 17:01:48 -0700

    Refactor client side code from prev proj

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,6 +7,6 @@ import '../dist/main.css';
 
 //get id from params to pass into the prop
 
-// ReactDOM.render(<App album={album} />, document.getElementById('rec-albums'));
+ReactDOM.render(<RecommendedAlbumsApp />, document.getElementById('rec-albums'));
 
-window.RecommendedAlbumsApp = RecommendedAlbumsApp;
+// window.RecommendedAlbumsApp = RecommendedAlbumsApp;
